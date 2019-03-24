290c67d3f6c2d36c34fcde7c1acf0989693e9f49
simon987 <fortier.simon@protonmail.com>
2019-03-24 12:58:47 -0400

    Fix deploy

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,6 +4,6 @@ export WSBROOT="ws_bucket"
 
 screen -S ws_bucket -X quit
 echo "starting ws_bucket"
-screen -S ws_bucket -d -m bash -c "cd ${WSBROOT} && chmod +x ws_bucket && export WS_BUCKET_SECRET=${WS_BUCKET_SECRET} && ./ws_bucket"
+screen -S ws_bucket -d -m bash -c "cd ${WSBROOT} && chmod +x ws_bucket && source ~/.bashrc && ./ws_bucket"
 sleep 1
 screen -list
